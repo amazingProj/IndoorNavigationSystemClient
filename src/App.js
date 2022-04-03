50c8a7169ca9e45b69bb18128e859fe2e0991b38
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-04-03 16:55:31 +0300

    client is working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from "react";
-import FourthFloor from './fourthFloor';
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
+  const userRef = useRef();
   const [response, setResponse] = useState("");
-
+ 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("94:B9:7E:FA:92:14", data => {
+      let wifiInformation = JSON.parse(JSON.stringify(data));
       setResponse(data);
       console.log(data)
+      userRef.current.style.left = wifiInformation["x"] + "px";
+      userRef.current.style.top = wifiInformation["y"] + "px";
+      
     });
   }, []);
-
+ 
   return (
-    <div/>
-     
+    <div>
+    <div ref={userRef} className="user"></div>
+    </div>
   );
 }
 
