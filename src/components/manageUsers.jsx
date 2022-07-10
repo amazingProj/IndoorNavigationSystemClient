7ce9a779bb3a242df21319270973cf1bd2e55021
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-07-10 17:56:57 +0300

    upgrading it

--- src/components/manageUsers.jsx
+++ src/components/manageUsers.jsx
@@ -114,21 +114,24 @@ const ManageUsers = (props) => {
             <div>
               <div className="flex items-stretch">
                 <li className="large-font">
-                  <input
-                    ref={rename}
-                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-last-name"
-                    type="text"
-                    placeholder={client.name}
-                  />
+                  <div className="flex">
+                    <input
+                      ref={rename}
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-last-name"
+                      type="text"
+                      placeholder={client.name}
+                    />
+                    <button
+                      onClick={() => {
+                        renameClient(client._id, client);
+                      }}
+                    >
+                      {renameMsg}
+                    </button>
+                  </div>
+
                   <br />
-                  <button
-                    onClick={() => {
-                      renameClient(client._id, client);
-                    }}
-                  >
-                    {renameMsg}
-                  </button>
                 </li>
               </div>
               <div className="large-font">
@@ -139,9 +142,11 @@ const ManageUsers = (props) => {
                 onClick={() => {
                   deleteClient(client._id);
                 }}
-                className="font"
+                className="mr-40 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font"
               >
-                {deleteMsg}
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  {deleteMsg}
+                </span>
               </button>
             </div>
           </div>
